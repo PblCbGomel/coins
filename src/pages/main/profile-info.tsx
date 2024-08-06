0388550e36b297c66e0f1f7834f81a46ad15cec0
PblCbGomel <pblcb.gomel@gmail.com>
2024-08-06 21:31:43 +0300

    Revert "fix: remarks"
    This reverts commit 7672e4a072485fd4c1b03da7cacc4223cb308427.

--- src/pages/main/profile-info.tsx
+++ src/pages/main/profile-info.tsx
@@ -1,16 +1,22 @@
 import { NavLink } from "react-router-dom";
 import { tg } from "../../App";
 import { Avatar } from "../../components/avatar/avatar";
-import { UserInfo } from "../../interfaces/user";
 
-export function ProfileInfo({ userInfo }: { userInfo: UserInfo | undefined }) {
+export function ProfileInfo() {
   return (
     <div className="profile-info">
       <div className="profile-tg-info">
-        <Avatar size="66px" name={userInfo?.name || "username"} />
+        <Avatar
+          size="66px"
+          name={tg?.initDataUnsafe?.user?.username || "username"}
+        />
         <div className="profile-text">
-          <div className="nickname">{userInfo?.name || "username"}</div>
-          <div className="id">id{userInfo?.tgId || "123456789"}</div>
+          <div className="nickname">
+            {tg?.initDataUnsafe?.user?.username || "username"}
+          </div>
+          <div className="id">
+            id{tg?.initDataUnsafe?.user?.id || "123456789"}
+          </div>
         </div>
       </div>
       <NavLink className="change-theme-btn" to={"/friends"}>
