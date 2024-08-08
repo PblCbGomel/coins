c63f2f0f09a46098a4aeeed5bc1afc17a8a8bedc
PblCbGomel <pblcb.gomel@gmail.com>
2024-08-08 17:50:15 +0300

    fix ref link

--- src/pages/friends/modal-buttons.tsx
+++ src/pages/friends/modal-buttons.tsx
@@ -47,7 +47,7 @@ export function ModalButtons({
           className="copy-link-btn"
           onClick={() => {
             navigator.clipboard.writeText(
-              `https://t.me/test002949402bot?start=${
+              `https://t.me/coinstest1123bot?start=${
                 tg?.initDataUnsafe?.user?.id || "123456789"
               }`
             );
