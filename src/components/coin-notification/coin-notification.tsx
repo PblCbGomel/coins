185cb216986f0e7bcde6a425d258d03a8ae2aa37
Evgeniy Shevtsov <yevgeniy.shevtsov.pblcb@mail.ru>
2024-08-17 00:00:08 +0300

    fix: img

--- src/components/coin-notification/coin-notification.tsx
+++ src/components/coin-notification/coin-notification.tsx
@@ -6,7 +6,7 @@ export function CoinNotification() {
   const { user } = useContext(UserContext);
   return (
     <div className={`notification-coins`}>
-      <img src="../icons/notif.svg" />
+      <img src="../icons/notif.svg" width={16} height={16} />
       <p>You Got +{user?.earnedCoins} Peak Points</p>
     </div>
   );
--- src/components/notification/notification.tsx
+++ src/components/notification/notification.tsx
@@ -3,7 +3,7 @@ import "./notification.css";
 export function Notification({ text }: { text: string }) {
   return (
     <div className={`notification`}>
-      <img src="../icons/notif.svg" />
+      <img src="../icons/notif.svg" width={16} height={16} />
       {text}
     </div>
   );
